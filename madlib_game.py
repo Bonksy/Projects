7345c1619f773459aa07859bd4d933e9af4f7831
Bonksy <takyiakwah@outlook.com>
2023-01-30 21:06:14 +0000

    Create madlib_game.py

---
+++ madlib_game.py
@@ -0,0 +1,10 @@
+adjective1 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input ("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter another adjective: ")
+
+print(f"Today I travelled to {adjective1}")
+print(f"When I got there I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
